7b4f8297b1b70e0ec8378c5f4d7b70f3ea92fe20
Arvind1809 <73241114+Arvind1809@users.noreply.github.com>
2024-05-22 15:53:33 +0530

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //int[] a = new int[2];
+            //a[0]= 1;
+            //a[1]= 2;
+            ////a[2]= 3;
+
+            //int[] b = new int[]{ 23, 25, 27, 34};
+
+            ////for (int i = 0; i < a.Length; i++)
+            ////{
+            ////    Console.WriteLine(a[i]);
+            ////}
+
+            //foreach (int i in a)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            //string[] students = new string[3];
+            //students[0] = "sajid";
+            //students[1] = "sujit";
+            //students[2] = "shubham";
+
+            //foreach (string s in students)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //Console.WriteLine("Enter the size of array: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] a = new int[size];
+
+            //Console.WriteLine("Enter the elements of array: ");
+            //for (int i=0; i < a.Length; i++)
+            //{
+            //    a[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Elements of array are: ");
+            //foreach (int i in a)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int? a = null;
+            string s = null;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
